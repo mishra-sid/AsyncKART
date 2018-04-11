39fcc52fdc6af570ccb27078cdbded1064d54132
sid <es15btech11018@iith.ac.in>
2018-04-11 18:25:03 +0530

    updated env code

--- async-kart.py
+++ async-kart.py
@@ -6,4 +6,17 @@ import gym_mupen64plus
 if __name__ == "__main__":
     env = gym.make('Mario-Kart-Royal-Raceway-v0')
 
-    env.render()
+    obs = env.reset()
+
+    end_episode = False
+    while not end_episode:
+        action = [63, 3, 3, 1, 1, 0]
+        obs, reward, end_episode, info = env.step(action)
+        print obs
+        print reward
+        print end_episode
+        print info 
+        env.render()
+    
+    obs = env.reset()
+    env.close()
